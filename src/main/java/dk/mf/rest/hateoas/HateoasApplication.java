72781dd82e7da7e971db6940d541f22a3d792eac
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-10-01 08:28:14 +0200

    Project files startup

---
+++ src/main/java/dk/mf/rest/hateoas/HateoasApplication.java
@@ -0,0 +1,13 @@
+package dk.mf.rest.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasApplication.class, args);
+    }
+
+}
